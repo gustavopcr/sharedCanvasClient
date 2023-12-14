a26813ed372a2ce89e2473bb65171e5117fdab90
unknown <gustavopc2501@gmail.com>
2023-12-14 01:39:38 -0300

    sending UDP messages

--- SharedCanvasClient/src/TCPHandler.java
+++ SharedCanvasClient/src/TCPHandler.java
@@ -103,54 +103,6 @@ public class TCPHandler {
 			e.printStackTrace();
 		}
 	}
-	/*
-	public static void main(String[] args) {
-        try {
-            Socket socket = new Socket("localhost", 9999); // Connect to server on localhost and port 9999
-
-            // Get the input and output streams for byte-oriented communication
-            OutputStream outToServer = socket.getOutputStream();
-            InputStream inFromServer = socket.getInputStream();
-
-            // Send bytes to the server
-            //byte[] bytesToSend = "Hello, Server!".getBytes(); // Convert string to bytes
-            
-            // Four integers to be sent
-            int number1 = 3000;
-            int number2 = 4150;
-            int number3 = 134;
-            int number4 = 1025;
-
-            // Convert integers to byte array
-            byte[] bytesToSend = intArrayToBytes(new int[]{number1, number2, number3, number4});
-
-            outToServer.write(bytesToSend);
-            outToServer.flush(); // Flush the output stream
-
-            // Receive bytes from the server
-            byte[] receivedBytes = new byte[4*4]; // Adjust buffer size as needed
-            int bytesRead = inFromServer.read(receivedBytes); // Read bytes into the buffer
-             if (bytesRead == Integer.BYTES * 4) {
-                // Convert bytes back to individual integers
-                int[] receivedIntegers = bytesToIntArray(receivedBytes);
-
-                // Print received integers
-                System.out.print("Received integers: ");
-                for (int num : receivedIntegers) {
-                    System.out.print(num + " ");
-                }
-                System.out.println();
-            } else {
-                System.out.println("Insufficient bytes received for 4 integers.");
-            }
-
-            // Close the socket
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-*/
     // Utility method to convert an array of integers to bytes
     private byte[] intArrayToBytes(int[] numbers) {
         ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
